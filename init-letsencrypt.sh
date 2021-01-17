18452ae0b26309636cd22abd9ecb21bea4c9cd38
Josh Hills <joshuamarcushills@gmail.com>
2021-01-18 00:07:35 +0100

    Altered pathing

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(joshhills.dev www.joshhills.dev staging.joshhills.dev)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="/var/nginx-data/data/certbot"
 email="joshuamarcushills@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
