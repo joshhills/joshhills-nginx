b2285538adcb47ca290ea4cb936a99f22b3f3aaf
Josh Hills <joshuamarcushills@gmail.com>
2021-03-15 18:54:24 +0100

    Disabled staging mode

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(joshhills.dev staging.joshhills.dev project.joshhills.dev www.project.j
 rsa_key_size=4096
 data_path="/etc/letsencrypt"
 email="joshuamarcushills@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
